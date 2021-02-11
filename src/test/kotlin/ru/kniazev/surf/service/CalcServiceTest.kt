d78f5e22dac67bdd46d451a00c8460edaa197093
Roman Kniazev <kniazevroman@gmail.com>
2021-02-11 20:12:23 +0300

    Add test for CalcService

---
+++ src/test/kotlin/ru/kniazev/surf/service/CalcServiceTest.kt
@@ -0,0 +1,39 @@
+package ru.kniazev.surf.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import ru.kniazev.surf.SurfApplication
+import ru.kniazev.surf.repository.CalcRepository
+import ru.kniazev.surf.repository.UserRepository
+import java.time.LocalDate
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(classes = [SurfApplication::class])
+internal class CalcServiceTest {
+    @Autowired
+    lateinit var service: CalcService
+    @Autowired
+    lateinit var repCalc: CalcRepository
+
+    @Test
+    fun `Check adding to repository`() {
+        val allRepBefore = repCalc.findAll()
+        service.start("2+2+2","surf")
+        val allRepAfter = repCalc.findAll()
+        val result = allRepAfter.minus(allRepBefore)
+        assertEquals(1, result.size)
+
+        assertEquals(LocalDate.now(), result[0].date)
+        assertEquals("2+2+2", result[0].request)
+        assertEquals("6", result[0].response)
+        assertEquals("surf", result[0].user?.login)
+    }
+
+
+
+}
